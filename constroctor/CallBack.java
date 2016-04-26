94102ccc523f452b3610020ba0acd5fb44353280
shinkook.kim <shinkook.kim@lge.com>
2016-04-26 17:11:59 +0900

    example constructor

---
+++ constroctor/CallBack.java
@@ -0,0 +1,17 @@
+public class CallBack {
+    public static void main(String[] args) {
+	childC c = new childC();
+		    
+    }
+}
+
+class superC {
+	superC(int a){
+		    
+		}
+}
+
+class childC extends superC{
+
+	childC(int a) { }
+}
