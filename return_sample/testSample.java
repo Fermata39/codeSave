e7972422974fdc95f122f0ebb649add14cf1463c
shinkook.kim <shinkook.kim@lge.com>
2016-05-12 11:16:06 +0900

    [Java] return sample code

---
+++ return_sample/testSample.java
@@ -0,0 +1,25 @@
+
+public class testSample {
+	public static void main(String[] args) {
+
+		    boolean flag = true;
+			    System.out.println("test start");
+				    testSample t = new testSample();
+
+					    t.sample(flag);
+						}
+
+	    public void sample(boolean flag) {
+			System.out.println("sample start");
+				if (flag) {
+						flag = false;
+							    if (flag == false) {
+										return;
+											    } else {
+														System.out.println("test");
+															    }
+								    }
+					System.out.println("test-2");
+					    }
+}
+
