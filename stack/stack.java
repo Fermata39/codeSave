04f37b1dfca4696b161ee05f889ab5d300eacb2c
shinkook.kim <shinkook.kim@lge.com>
2016-12-03 10:49:58 +0900

    modify line

--- stack/stack.java
+++ stack/stack.java
@@ -3,61 +3,61 @@ import java.util.Scanner;
 
 public class main {
 
-	/**
-	     * @param args
-		 */
-	public static void main(String[] args) {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
 
-		    Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-			    String input = sc.nextLine();
-				    int i;
-					    stack st = new stack();
-						    StringBuilder sb = new StringBuilder();
-							    
-							    for(i =0; i<input.length(); i++){
-									    st.push(String.valueOf(input.charAt(i)));
-											//sb.append(st.pop());
-										    }
-								    for(int j =0; j<input.length(); j++){
-										    sb.append(st.pop());
-												//sb.append(st.pop());
-											    }
-									    
-									    //System.out.println("res: " + st.pop());
-									    System.out.println("res: " + sb.toString());
-										}
+        String input = sc.nextLine();
+        int i;
+        stack st = new stack();
+        StringBuilder sb = new StringBuilder();
+
+        for(i =0; i<input.length(); i++){
+            st.push(String.valueOf(input.charAt(i)));
+            //sb.append(st.pop());
+        }
+        for(int j =0; j<input.length(); j++){
+            sb.append(st.pop());
+            //sb.append(st.pop());
+        }
+
+        //System.out.println("res: " + st.pop());
+        System.out.println("res: " + sb.toString());
+    }
 
 }
 
 class stack {
-	String sd[] = new String[100];
-	    int size;
-		
-		stack() {
-			    size = 0;
-				}
+    String sd[] = new String[100];
+    int size;
 
-		    String pop() {
-				String rs = sd[size - 1];
-					size--;
-						return rs;
-						    }
+    stack() {
+        size = 0;
+    }
 
-			void push(String data) {
-				    sd[size] = data;
-					    size++;
-						}
+    String pop() {
+        String rs = sd[size - 1];
+        size--;
+        return rs;
+    }
 
-			    boolean empty() {
-					if (size == 0) {
-							return true;
-								} else {
-										return false;
-											}
-					    }
+    void push(String data) {
+        sd[size] = data;
+        size++;
+    }
 
-				int top() {
-					    return size;
-						}
+    boolean empty() {
+        if (size == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    int top() {
+        return size;
+    }
 }
