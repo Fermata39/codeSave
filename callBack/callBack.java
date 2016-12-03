9c86ff3ad2349498c4f7c5b9f1e336c439390b7d
shinkook.kim <ksbang39@gmail.com>
2016-12-03 23:40:19 +0900

    modify commit

--- callBack/callBack.java
+++ callBack/callBack.java
@@ -1,37 +1,37 @@
 public class callBack {
 	public static void main(String[] args) {
-		    ifcallback ifc = null;
-			    implCallback ic = new implCallback(ifc);
-				    ic.testCallFunc();
-					}
+		ifcallback ifc = null;
+		implCallback ic = new implCallback(ifc);
+		ic.testCallFunc();
+	}
 }
 
 class implCallback implements ifcallback {
 
 	ifcallback ifc;
 
-	    public implCallback(ifcallback ifc) {
-			// TODO Auto-generated constructor stub
-			this.ifc = new ifcallback() {
-					
-					@Override
-							public void testCallback() {
-									    // TODO Auto-generated method stub
-									    System.out.println("impl testCallback");
-											}
-						};
+	public implCallback(ifcallback ifc) {
+		// TODO Auto-generated constructor stub
+		this.ifc = new ifcallback() {
 
-			    }
-
-		public void testCallFunc() {
-			    ifc.testCallback();
-				}
-
-		    @Override
-			    public void testCallback() {
+			@Override
+				public void testCallback() {
 					// TODO Auto-generated method stub
-					
-				    }
+					System.out.println("impl testCallback");
+				}
+		};
+
+	}
+
+	public void testCallFunc() {
+		ifc.testCallback();
+	}
+
+	@Override
+		public void testCallback() {
+			// TODO Auto-generated method stub
+
+		}
 
 }
 
