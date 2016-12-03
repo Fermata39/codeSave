a17247f1dc90345abc74ab7efed6ab36a5d5f9d4
shinkook.kim <shinkook.kim@lge.com>
2016-12-03 10:49:58 +0900

    modify line

--- initial_call/init.java
+++ initial_call/init.java
@@ -1,39 +1,39 @@
 
 public class init {
 
-	/**
-	     * @param args
-		 */
-	public static void main(String[] args) {
-		    // TODO Auto-generated method stub
-		    Child ch = new Child();
-			}
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Child ch = new Child();
+    }
 
 }
 
 class Super {
-        static {
-	            System.out.println("Super static block");
-		        }
-	    {
-		        System.out.println("Super instance block");
-			    }
-	        Super() {
-		            System.out.println("Super generator");
-			        }
+    static {
+        System.out.println("Super static block");
+    }
+    {
+        System.out.println("Super instance block");
+    }
+    Super() {
+        System.out.println("Super generator");
+    }
 }
 class Child extends Super {
-        static {
-	            System.out.println("Child static block");
-		        }
-	    {
-		        System.out.println("Child instance block");
-			    }
-	        Child() {
-		            this(500);
-			            System.out.println("Child generator");
-				        }
-		    Child(int i) {
-			        System.out.println("Child generator with parameter");
-				    }
+    static {
+        System.out.println("Child static block");
+    }
+    {
+        System.out.println("Child instance block");
+    }
+    Child() {
+        this(500);
+        System.out.println("Child generator");
+    }
+    Child(int i) {
+        System.out.println("Child generator with parameter");
+    }
 }
