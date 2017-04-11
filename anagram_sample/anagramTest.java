429e06a26c2504231f3f382cbc9f2ceef3342183
shinkook.kim <ksbang39@gmail.com>
2017-04-12 00:04:02 +0900

    modify anagram for ascii case

--- anagram_sample/anagramTest.java
+++ anagram_sample/anagramTest.java
@@ -10,9 +10,26 @@ public class anagramTest{
         System.out.println("input String1:");
         String s2 = sc.nextLine();
 
-        boolean flag = isAnagram(s1,s2);
+        boolean flag = isAnagram2(s1,s2);
 
         System.out.println("result: " + flag);
+    }
+
+    public static boolean isAnagram2(String s1, String s2){
+      int[] flag = new int[128]; // if ascii 일 경우
+
+      for(int i=0; i<s1.length(); i++){
+        flag[s1.toLowerCase().charAt(i)]++;
+      }
+
+      for(int i=0; i<s2.length(); i++){
+        flag[s2.toLowerCase().charAt(i)]--;
+        if(flag[s2.charAt(i)] < 0 ){
+          return false;
+        }
+      }
+
+      return true;
     }
 
     public static boolean isAnagram(String s1, String s2){
@@ -22,11 +39,11 @@ public class anagramTest{
         String str1,str2;
         for(int i=0; i<s1.length();i++){
             str1 = s1.toLowerCase();
-            cnt1[s1.charAt(i)-'a']+=1;
+            cnt1[s1.charAt(i)-'a']++;
         }
         for(int i=0; i<s2.length();i++){
             str2 = s2.toLowerCase();
-            cnt2[s2.charAt(i)-'a']+=1;
+            cnt2[s2.charAt(i)-'a']++;
         }
         for(int i='a'; i<='z';i++){
             if(cnt1[i-'a'] != cnt2[i-'a']){
