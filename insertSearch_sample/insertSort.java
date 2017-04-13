c9ff0e9f4fc6f4e9e26a813a408b838a82e9fce4
shinkook.kim <ksbang39@gmail.com>
2017-04-13 15:16:15 +0900

    add comment for insert sort

--- insertSearch_sample/insertSort.java
+++ insertSearch_sample/insertSort.java
@@ -10,10 +10,10 @@ public class insertSort{
 
 	public static void insertSort(int[] ary){
 		for(int i=1; i<ary.length; i++){
-			int key = ary[i];
+			int key = ary[i]; // 기준 값 setting
 			for(int j=i-1; j>=0; j--){
-				if(ary[j] > key){
-					int temp = ary[j];
+				if(ary[j] > key){ // 기준 값과 scan 요소 대상 비교시 key 값이 작으면
+					int temp = ary[j]; // key 값을 스위칭
 					ary[j] = key;
 					ary[j+1] = temp;
 				}
