72aaea3bd07e03031f211269b87331e0360d48e8
shinkook.kim <ksbang39@gmail.com>
2017-04-14 17:52:08 +0900

    create bubble sample for java

---
+++ bubbleSort/bubble.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class bubble{
+	public static void main(String[] args){
+		int[] arr = {5,4,1,2,3,7,6,8,9,10};
+
+		bubbleSort(arr);
+	}
+
+	public static int[] swap(int[] arr, int i, int j){
+		
+		int tmp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = tmp;
+
+		return arr;
+	}
+	public static void bubbleSort(int[] arr){
+		
+		for(int i =0 ; i<arr.length; i++){
+			for(int j = i+1; j<arr.length; j++){
+				if(arr[i] > arr[j]){
+					arr = swap(arr,i,j);
+				}
+			}
+		}
+
+		System.out.println("Bubble: " + Arrays.toString(arr));
+	
+	}
+
+}
