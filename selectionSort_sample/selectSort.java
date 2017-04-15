f0aac2add9bc153ae0ef9f69214ba5d07fd23920
shinkook.kim <ksbang39@gmail.com>
2017-04-15 10:47:11 +0900

    modify selection sort

--- selectionSort_sample/selectSort.java
+++ selectionSort_sample/selectSort.java
@@ -5,8 +5,29 @@ public class selectSort{
 		int[] ary = {5,4,1,2,3};
 
 		callSelectionSort(ary);
+		callSelectionSort2(ary);
 	}
 
+	public static int[] swap(int[] arr, int i, int j){
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+		return arr;
+	}
+
+	public static void callSelectionSort2(int[] arr){
+		int min = 0;
+
+		for(int i=0; i<arr.length; i++){
+			min = i;
+			for(int j=i+1; j<arr.length;j++){
+				if(arr[min] > arr[j])
+					arr = swap(arr,j,min);
+			}
+		}
+
+		System.out.println("res2: " + Arrays.toString(arr));
+	}
 
 	public static void callSelectionSort(int[] ary){
 
