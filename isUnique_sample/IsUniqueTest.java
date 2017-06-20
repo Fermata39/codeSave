0ecc409a2cb807823804a02b249e3c6947e51d8e
shinkook.kim <shinkook.kim@lge.com>
2017-06-20 19:13:21 +0900

    create unique code_study

---
+++ isUnique_sample/IsUniqueTest.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class IsUniqueTest{
+    public static void main(String[] args){
+        boolean flag = isUnique("abcdd");
+        System.out.println("flag: " + flag);
+    }
+
+    public static boolean isUnique(String str){
+        char[] ca = str.toCharArray();
+        
+        Map<Character,Integer> map = new HashMap<Character,Integer>();
+        int num =0 ;
+        for(char c : ca){
+            if(!map.containsKey(c)){
+                map.put(c,num);
+                num++;
+            }else{
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
