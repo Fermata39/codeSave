e5e33e9a9c219c1e69b6bc912c3bc6c30e8a6ab7
shinkook.kim <shinkook.kim@lge.com>
2017-07-24 10:53:44 +0900

    modified swap Test code , no temp variable - add main func() - add commnet for code

--- swap_sample/SwapTest.java
+++ swap_sample/SwapTest.java
@@ -1,24 +1,27 @@
 import java.util.*;
 
 public class SwapTest {
-	public static void main(String[] args){
-		
-		int a[] = new int[10];
-		callSwap(4,10,a);
+    public static void main(String[] args){
 
-		//System.out.println("swap: " , a.toString);
-	}
+        int a[] = new int[11];
+        a[4] = 4;
+        a[10] = 10;
 
-	public static void callSwap(int i,int k,int[] a){
-		// i = 4 , k = 10
-		
-		// a[i] == 6
-		a[i] = a[k] - a[i];
-		// a[k] == 4
-		a[k] = a[k] - a[i];
-		
-		a[i] = a[k] + a[i];
+        System.out.println("a[i]:" + a[4] + "/ a[k]: "+a[10]);
 
-		System.out.println("a[k]:" + a[k] + "a[i]: "+a[i]);
-	}
+        callSwap(4,10,a);
+    }
+
+    public static void callSwap(int i,int k,int[] a){
+        // i = 4 , k = 10
+
+        // a[i] == 6
+        a[i] = a[k] - a[i];
+        // a[k] == 4
+        a[k] = a[k] - a[i];
+        // a[i](6) + a[k](4) = a(i)10
+        a[i] = a[k] + a[i];
+
+        System.out.println("a[i]:" + a[i] + "/ a[k]: "+a[k]);
+    }
 }
